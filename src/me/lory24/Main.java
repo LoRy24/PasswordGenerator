4a3f6b411f81dbb372e2c545fa5c74dfd600e67b
LoRy24 <lorytherock24tutorial@gmail.com>
2021-07-18 23:42:13 +0200

    Removed default comment

--- src/me/lory24/Main.java
+++ src/me/lory24/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) { // write your code here
+    public static void main(String[] args) {
         System.out.println("Welcome to Password generator!");
         while (true) {
             try {
